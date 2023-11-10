2fc5ec2f5e4d11bd3ab1d42dc8190f78e91251a2
some_rando <90100321+RandomLegend3@users.noreply.github.com>
2023-11-10 03:14:27 -0500

    Update dcerpc_method_abuse_notes.py
    added type hinting.  added att&ck ttp and attack type outputs
    
    Signed-off-by: some_rando <90100321+RandomLegend3@users.noreply.github.com>

--- src/dcerpc_method_abuse_notes.py
+++ src/dcerpc_method_abuse_notes.py
@@ -1,33 +1,42 @@
-from make_colorful import Color
-# Import custom Color class to enable colored text output
+from typing import Tuple, Optional, Dict, Any, Union
 
-from dcerpc_data import dcerpc_services
-# Import custom function to fetch and detail DCERPC services, highlighting possible and known abuse
-
-from make_helpers import get_input_opnum
-# Import custom function that validates user input and returns int, in order to map to the corresponding
-# method and notes
+from make_colorful import Color           # Import custom Color class to enable colored text output
+from dcerpc_data import dcerpc_services   # Import MSRPC to ATT&CK lookup table
+from make_helpers import get_input_opnum  # Import custom function that validates user input and returns int
 
 
-def get_dcerpc_info(service_name, opnum):
+def get_dcerpc_info(service_name: str, opnum: int) -> Union[tuple[None, str], tuple[Any, Any, Any, Any]]:
     """
     Retrieves information about the operation number (opnum) and associated method
     name for a given DCERPC service.
 
     Args:
-        service_name (str): The name of the service to query (e.g., "SAMR").
+        service_name (str): The name of the service to query (e.g., "samr").
         opnum (int): The operation number for which to retrieve the method info.
-
-    Returns:
-        tuple: A tuple containing the method name and note associated with the opnum,
-               or (None, None) if not found.
     """
-    service = dcerpc_services.get(service_name.lower())
+    service: Optional[Dict[str, Any]] = dcerpc_services.get(service_name.lower())
     if not service:
-        return None, f"Service {service_name} not found."
+        return None, f"{Color.LIGHTRED}Service {service_name} not found.{Color.END}", None, None
 
-    method_info = service["Methods"].get(opnum)
+    method_info: Optional[Dict[str, int]] = service["Methods"].get(int(opnum))
     if method_info:
-        return method_info["Method"], method_info["Note"]
+        return (
+            method_info["Method"],
+            method_info["Note"],
+            method_info["ATT&CK TTP"],
+            method_info['Attack Type']
+        )
     else:
-        return None, f"Method with opnum {opnum} not found in service {service_name}."
+        return None, f"Method with opnum {opnum} not found in service {service_name}.", None, None
+
+
+if __name__ == '__main__':
+    service_name_input: str = 'lsarpc'
+    opnum_input: int = 76
+    results: Tuple[Optional[str], Optional[str], Optional[str], Optional[str]] = get_dcerpc_info(service_name_input,
+                                                                                                 opnum_input)
+
+    print(results[0])  # Method
+    print(results[1])  # Note
+    print(results[2])  # ATT&CK TTP
+    print(results[3])  # Attack Type
