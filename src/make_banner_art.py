1ee5a5157dbe057a29a62eea8482b9322887774e
some_rando <90100321+RandomLegend3@users.noreply.github.com>
2023-11-13 22:23:00 -0500

    Update make_banner_art.py (#44)
    added module docstrings
    
    Signed-off-by: some_rando <90100321+RandomLegend3@users.noreply.github.com>

--- src/make_banner_art.py
+++ src/make_banner_art.py
@@ -1,3 +1,33 @@
+"""  # pylint: disable=line-too-long
+This module contains the functionality for generating a decorative banner art for a Python-based TShark wrapper
+application. The banner art, created using various ASCII characters and symbols, adds a unique and creative touch
+to the command-line interface of the application.
+
+Author: 4u7h0r (80y13)
+Created: July 2023
+
+Purpose:
+The module is part of a larger Python wrapper over TShark, which allows users to analyze PCAP files through a
+user-friendly command-line interface. It enhances user experience by providing a visually appealing banner at
+the start of the application.
+
+Usage:
+The `banner` variable, defined as a raw formatted string, can be printed to the console to display the art. It
+includes colorful elements and symbols to create an engaging visual.
+
+Dependencies:
+- The module relies on the `make_colorful` module for colorizing certain parts of the banner.
+
+Compatibility:
+This script is compatible with WSL2 Ubuntu, macOS, and other Unix-like systems.
+
+Note:
+The script provides references to Wireshark display filter documentation and sources for downloading PCAP files.
+It's designed to be part of a larger application that provides network packet analysis functionalities.
+
+This module is purely for aesthetic purposes and does not contain any functional logic related to packet analysis.
+"""
+
 from make_colorful import Color
 
 
@@ -9,7 +39,7 @@ banner = rf"""
   /              \/*
  |                |     (r3473d: Ju1y, 2023
 |.-.           _.-.|
-|| |          |   ||    7h!5 5(r!p7 !5 3553n7!411y 4 Py7h0n wr4pp3r 0v3r 75h4rk wh!(h 1375 y0u !n73r4(7!v31y hun7 7hr0u9h 4 P(4P f!13.
+|| |          |   ||    7h!5 !5 4 Py7h0n wr4pp3r 0v3r 75h4rk wh!(h 1375 y0u hun7 7hr0u9h 4 P(4P f!13.
 || |   -:-    |   ||
 || |.-'-' '-.-|   ||    7h!5 5(r!p7 w!11 f!r57 45k f0r 7h3 fu11 p47h 70 7h3 P(4P f!13.
  \__|        \__/
