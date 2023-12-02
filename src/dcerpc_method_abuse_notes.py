91bf2eb40f237acfe7c9d212ce243f78750d57b5
some_rando <90100321+z0her0@users.noreply.github.com>
2023-12-02 08:23:39 -0500

    Update dcerpc_method_abuse_notes.py (#109)
    added enhanced search capabilities
    
    Signed-off-by: some_rando <90100321+z0her0@users.noreply.github.com>

--- src/dcerpc_method_abuse_notes.py
+++ src/dcerpc_method_abuse_notes.py
@@ -1,48 +1,125 @@
-"""  # pylint: disable=line-too-long
-This module provides functionality to retrieve detailed information about specific methods within
-Distributed Computing Environment / Remote Procedure Calls (DCE/RPC) services. It utilizes a
-dictionary of known DCE/RPC services and their methods, each identified by an operation number (opnum).
+"""
+Purpose: Retrieves specific information about DCERPC services based on user input (service name and operation number).
 
-The main functionality is encapsulated in the `get_dcerpc_info` function. This function accepts a service
-name and an operation number as input, and returns detailed information about the corresponding method
-within the specified DCE/RPC service. This information includes the method's name, a note describing
-the method, any associated attack techniques, tactics, and procedures (TTPs), the type of attack
-associated with the method, and indicators of compromise (IOCs).
+Functionality:
+  The get_dcerpc_info function fetches detailed information for a given service and its operation number.
+  It also has the capability to list all available services if list_services is set to True.
 
-This module is particularly useful in the context of network security and forensic analysis, where
-understanding the details of DCE/RPC methods can be crucial for identifying potential malicious activities.
-
-Functions:
-    - `get_dcerpc_info`: Retrieves information about a specific opnum for a given DCE/RPC service.
+Integration: Imports dcerpc_services from dcerpc_data.py to access the data dictionary.
 """
 
-from typing import Tuple, Union
+from typing import Tuple, Union, List, Dict, Any
 
 from make_colorful import Color
 from dcerpc_data import dcerpc_services
 
 
-# pylint: disable=line-too-long
-def get_dcerpc_info(service_name: str, opnum: int) -> Union[Tuple[None, str, None, None, None], Tuple[int, int, int, int, int]]:
+def display_summary_detail(data: Dict[str, Any], key: str) -> None:
+    """
+    Displays a detailed view of a specific summary key from the DCERPC data.
+
+    This function checks if the given key is present in the 'summary' section of the data.
+    If found, it prints the detailed information for that key, otherwise it prints an error message.
+    """
+    if "summary" in data and key in data["summary"]:
+        summary_detail = data["summary"][key]
+        print(f"\n{Color.BOLD + Color.AQUA}{key.capitalize()} Detail{Color.END}:\n{Color.GREY}{summary_detail}{Color.END}\n")
+    else:
+        print(f"No detailed data available for '{key}'.")
+
+
+def format_method_details(method_details):
+    """
+    Formats the method details for display.
+
+    This function iterates over the key-value pairs in the method details, applying color formatting
+    and adjusting the indentation for better readability.
+    """
+    formatted_details = []
+    for key, value in method_details.items():
+        formatted_value = value.replace("\n", "\n         ")
+        key_color = Color.BOLD + Color.SKY_BLUE
+        value_color = Color.GREY
+        formatted_details.append(f"  {key_color}{key}{Color.END}:  {value_color}{formatted_value}{Color.END}")
+
+    return "\n".join(formatted_details)
+
+
+def list_methods(service_name: str, data: Dict[str, Any]) -> None:
+    """
+    Lists all the methods available for a specified DCERPC service.
+
+    This function checks if the specified service exists and has methods defined in the provided data.
+    If so, it prints the list of methods with their details, else prints an error message.
+    """
+    # Check if the specified service exists in the data and is a dictionary
+    if service_name in data and isinstance(data[service_name], dict):
+        service_data = data[service_name]
+
+        # Check if the service has methods defined
+        if "Methods" in service_data and isinstance(service_data["Methods"], dict):
+            # Apply color formatting for the service name
+            service_name_color = Color.BOLD + Color.GOLD
+            print(f"\n{service_name_color}Methods in {service_name}{Color.END}:\n")
+
+            # Iterate through each method and display its details
+            for method_id, method_details in service_data["Methods"].items():
+                # Apply color formatting for the method ID
+                method_id_color = Color.BOLD + Color.CYAN
+                print(f"{method_id_color}- Opnum {method_id}{Color.END}:\n{format_method_details(method_details)}\n")
+        else:
+            # If no methods are found for the service, display a message in red
+            no_methods_color = Color.BOLD + Color.LIGHTRED
+            print(f"{no_methods_color}No methods found for {service_name}.{Color.END}")
+    else:
+        # If the service is not found in the data, display a message in red
+        service_not_found_color = Color.BOLD + Color.LIGHTRED
+        print(f"{service_not_found_color}Service {service_name} not found.{Color.END}")
+
+
+def enhanced_search(keyword: str, data: Dict[str, Any]) -> List[str]:
+    """
+    Enhanced search in the dcerpc_services dictionary.
+    """
+    keyword_lower = keyword.lower()
+    results = []
+
+    # Define colors for different parts of the search results
+    service_color = Color.BOLD + Color.AQUA
+    method_color = Color.BOLD + Color.YELLOW
+    detail_color = Color.GREEN
+    text_color = Color.GREY
+
+    for service_name, service_data in data.items():
+        # Check if the service data is a dictionary and contains the 'Methods' key
+        if isinstance(service_data, dict) and "Methods" in service_data:
+            for method_id, method_details in service_data["Methods"].items():
+                # Check if any part of the method details contains the keyword (case-insensitive)
+                if any(keyword_lower in str(value).lower() for value in method_details.values()):
+                    # Format the service name and method ID with color
+                    result = f"{service_color}Service: {service_name}, {method_color}Method {method_id}:{Color.END}"
+                    # Iterate through each detail key-value pair
+                    for key, value in method_details.items():
+                        # Format the value with indentation and color
+                        formatted_value = value.replace("\n", f"\n{text_color}    ")
+                        # Append the formatted detail to the result string
+                        result += f"\n  {detail_color}{key}: {text_color}{formatted_value}{Color.END}"
+                    # Add the fully formatted result to the results list
+                    results.append(result)
+
+    return results
+
+
+def get_dcerpc_info(service_name: str = "", opnum: int = 0, list_services: bool = False) -> Union[Tuple[None, str, None, None, None], Tuple[int, int, int, int, int], List[str]]:
     """
     Retrieves information about a specific operation number (opnum) for a given DCERPC service.
-
-    Args:
-        service_name (str): The name of the DCERPC service.
-        opnum (int): The operation number for which information is requested.
-
-    Returns:
-        Union[Tuple[None, str, None, None, None], Tuple[str, str, str, str, str]]:
-        A tuple containing information about the DCERPC method, or an error message if the service or method is not
-        found.
-        The tuple format is (Method, Note, Attack_TTP, Attack_Type, IOC) for valid cases,
-        or (None, error message, None, None, None) for invalid cases.
-
-    Notes:
-        This function first checks if the provided service name exists in the `dcerpc_services` dictionary.
-        If the service is found and valid, it then looks for the method associated with the given opnum.
-        If the method is found, its details are returned; otherwise, an error message is provided.
     """
+    if list_services:
+        return [service for service in dcerpc_services.keys() if isinstance(dcerpc_services[service], dict)]
+
+    if service_name is None or opnum is None:
+        return None, "Service name or opnum not provided.", None, None, None
+
     service = dcerpc_services.get(service_name.lower())
 
     if service is None or isinstance(service, str) or not isinstance(service, dict) or "Methods" not in service:
